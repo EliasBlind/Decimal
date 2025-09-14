21cfa28adea43f4c1e05bf987cdbbc39181a87aa
marjoryc <eliaseromanov@gmail.com>
2025-09-14 15:09:11 +0700

    Base struct

---
+++ headers/decimaldef.h
@@ -0,0 +1,20 @@
+#ifndef DECIMALDEF_H
+
+#define OK 0
+
+// Arithmetic operations
+#define NEGATIVE_INFINITY 1
+#define INFINITY 2
+#define DIVISION_BY_ZERO 3
+
+// Comparision operations
+#define FALSE 0
+#define TRUE 1
+
+// Converters
+#define CONVERSION_ERROR 1
+
+// Other functions
+#define CALCULATION_ERROR 1
+
+#endif // DECIMALDEF_H
